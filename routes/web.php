86c866ec323de0ddfcd76754237cbbd1e979edb5
Francesco Iorio <ioriofre_99@hotmail.it>
2024-11-19 18:15:48 +0100

    tested create-read records routes

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,22 @@ Route::get('/home', function () {
                   ->header('Content-Type', 'text/plain');
 });
 
+// Create new record
 Route::post('/new/record', function (Request $request) {
+    $input = $request->input();
     DB::insert('insert into records (type, account, amount, description) values (?, ?, ?, ?)', 
-                [$request->all()['type'], 
-                $request->all()['account'], 
-                $request->all()['amount'], 
-                $request->all()['description']]
+                [$input['type'], 
+                $input['account'], 
+                $input['amount'], 
+                $input['description']]
     );
     return response('record correctly inserted', 200);
 })->name('newRecord');
+
+// Read all records
+Route::get('/get/records', function (Request $request) {
+    $response = DB::select('select * from records');
+    return response()->json($response);
+})->name('getRecords');
+
+

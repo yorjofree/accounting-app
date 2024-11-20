3df49b1dafba4a21a16736ce0228abb0d2bb3c51
Francesco Iorio <ioriofre_99@hotmail.it>
2024-11-20 16:38:47 +0100

    tested RecordController and Route::resource

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -3,10 +3,80 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RecordController extends Controller
 {
-    public function create() {
-        
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $response = DB::select('select * from records');
+        return response()->json($response);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $input = $request->input();
+        DB::insert('insert into records (type, account, amount, description) values (?, ?, ?, ?)', 
+            [$input['type'], 
+            $input['account'], 
+            $input['amount'], 
+            $input['description']]
+        );
+        return response('record correctly inserted', 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $input = $request->input();
+        DB::update('update records set type = ?, account = ?, amount = ?, description = ? where id = ?', 
+            [$input['type'],
+            $input['account'],
+            $input['amount'],
+            $input['description'],
+            $id]
+        );
+        return response('record updated', 200);    
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        DB::delete('delete from records where id = ?', [$id]);
+        return response(200);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,42 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Http\Controllers\RecordController;
 
-// Create new record
-Route::post('/new/record', function (Request $request) {
-    $input = $request->input();
-    DB::insert('insert into records (type, account, amount, description) values (?, ?, ?, ?)', 
-                [$input['type'], 
-                $input['account'], 
-                $input['amount'], 
-                $input['description']]
-    );
-    return response('record correctly inserted', 200);
-})->name('newRecord');
-
-// Read all records
-Route::get('/get/records', function (Request $request) {
-    $response = DB::select('select * from records');
-    return response()->json($response);
-})->name('getRecords');
-
-// Update record
-Route::put('/update/record', function (Request $request) {
-    $id = $request->query('id');
-    $input = $request->input();
-    DB::update('update records set type = ?, account = ?, amount = ?, description = ? where id = ?', 
-                [$input['type'],
-                $input['account'],
-                $input['amount'],
-                $input['description'],
-                $id]
-    );
-    return response('record updated', 200);
-})->name('updateRecord');
-
-// Delete record
-Route::delete('/delete/record', function (Request $request) {
-    $id = $request->query('id');
-    DB::delete('delete from records where id = ?', [$id]);
-    return response(200);
-})->name('deleteRecord');
+// Routing to RecordController
+Route::resource('records', RecordController::class);
